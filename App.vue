a9b25d44ad8d73de2b35800b3a59eefa2434878e
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-01-16 17:08:40 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app flat class="grey">
+    <Navbar />
+    <v-content class="mx-4 mb-4">
+      <router-view/>
+    </v-content>
+  
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {Navbar},
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+.grey {
+  background: #F5F5F5 !important;
+}
+</style>
